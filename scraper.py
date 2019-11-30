3a68923dbf5d9ba98ee76fb52e4f3efee9d822db
Neeraj Negi <neeraj.negi@loconav.com>
2019-11-30 17:11:11 +0530

    Initial commit

---
+++ scraper.py
@@ -0,0 +1,74 @@
+from selenium import webdriver
+import time
+import csv
+
+driver = webdriver.Firefox(executable_path = '/Users/new/Documents/projects/tpnagar/geckodriver') 
+url = "http://tpnagar.com/Search/Search?City=159~Ahmedabad&txtSearch=Transport%20Contractors"
+
+driver.get(url)
+time.sleep(3)
+
+last_transporter = ""
+pagination_xpath = '//div[contains(@class, "col-12") and contains(@class, "pagination")]/ul/li'
+next_btn_xpath = '//a[@id="next"]'
+container_xpath = '//div[contains(@class, "box") and contains(@class, "searchboxlist")]'
+
+# transporter_name_xpath = container_xpath + '/div[@class=top]'
+# transporter_address_xpath = container_xpath + '/div[@class=second]/p[1]'
+# transporter_numbers_xpath = container_xpath + '/div[@class=second]/p[2]/strong[1]'
+# transporter_address_xpath = container_xpath + '/div[@class=second]/p[2]/strong[2]'
+
+def get_btns():
+	return driver.find_elements_by_xpath(pagination_xpath)
+
+all_data = []
+containers = driver.find_elements_by_xpath(container_xpath)
+for c in containers:
+	data_row = c.text.split("\n")
+	if(data_row == ['']):
+		break
+	else:
+		this_page_last_data = data_row
+
+last_page_last_data = []
+
+while(last_page_last_data != this_page_last_data or len(all_data) == 0):
+	current_btn = 0
+	containers = driver.find_elements_by_xpath(container_xpath)
+	time.sleep(2)
+	next_btn = driver.find_elements_by_xpath(next_btn_xpath)
+	if(len(all_data) == 0):
+		last_page_last_data = ["bluu"]
+	else:
+		last_page_last_data = all_data[(len(all_data) - 1)]
+
+	for c in containers:
+		data_row = c.text.split("\n")
+		if(data_row == ['']):
+			current_btn += 1
+			all_btns = get_btns()
+			btns = all_btns[1:len(all_btns)]
+			if(current_btn < len(btns)):
+				print(".......BTN{current_btn} IN......{total}\n".format(current_btn = current_btn, total = len(btns)))
+				all_btns[current_btn].click()
+				time.sleep(2)
+				containers = driver.find_elements_by_xpath(container_xpath)
+				time.sleep(2)
+				pass
+			else:
+				print("\n\n....NEXT....\n\n")
+				next_btn[0].click()
+				time.sleep(2)
+				break		
+		else:
+			this_page_last_data = data_row
+			all_data.append(data_row)
+
+with open('data.csv', 'a', newline = '') as file:
+	writer = csv.writer(file)
+	writer.writerow(["Transporter", "Address", "Numbers", "Contact Person"])
+	for data in all_data:
+		writer.writerow(data)
+
+
+driver.close()
